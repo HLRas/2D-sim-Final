cec3486b039aaa805aef1d5521a84a07251420af
HLRas <herman.ras2003@gmail.com>
2025-10-20 16:14:06 +0200

    finished tank turn

--- main.py
+++ main.py
@@ -810,7 +810,7 @@ def find_closest(data, timestamp, index=2):
 def get_straighttime(pos_c, pos_d, speed=50):
     dy, dx = pos_d[1] - pos_c[1], pos_d[0] - pos_c[0]
     d = math.sqrt(dx**2 + dy**2)
-    straight_time = d/speed
+    straight_time = d/speed - 0.5
     return straight_time
 
 def get_tanktime(pos_c, orient_c:float, pos_d, thres_deg=5, turn_speed=50):
